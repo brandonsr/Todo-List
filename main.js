fed874a5567a7b7a5b1b00d740e47a21e7c61658
brandonsr <brandon2004roldan@gmail.com>
2021-01-27 20:52:54 -0600

    add the opcion for clean task's

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
 const todos = []; 
 
+const render = () => {
+        const todoList = document.getElementById('todo-list');
+    const TodosTemplate = todos.map( t => '<li>' + '<input type="checkbox">' + t + '</li>');
+    todoList.innerHTML = TodosTemplate.join('');
+    const elementos = document.querySelectorAll('#todo-list li');
+    elementos.forEach( (elemento, i) =>{
+        elemento.addEventListener('click', () =>{
+        elemento.parentNode.removeChild(elemento)
+        // Clean the array: 
+        todos.splice(i, 1);
+        render() 
+        })
+    }
+    )
+}
+
 const form = document.getElementById('todo-form');
 form.onsubmit = (x) =>{
     x.preventDefault();
@@ -7,9 +23,5 @@ form.onsubmit = (x) =>{
     const todotext = todo.value;
     todo.value = ''; 
     todos.push(todotext);
-    const todoList = document.getElementById('todo-list');
-    todoList.innerHTML = ''
-    for( i = 0; i < todos.length; i++){
-        todoList.innerHTML += '<li>' + '<input type="checkbox">' + todos[i] + '</li>'
+    render(); 
     }
-}
