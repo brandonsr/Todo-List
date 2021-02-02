39ebb50844a217d51a121a66a323ba9c72e03e06
brandonsr <brandon2004roldan@gmail.com>
2021-02-01 19:09:18 -0600

    added the localstorage for todos

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const todos = []; 
+const todos = JSON.parse(localStorage.getItem('todos')) || []; 
 
 const render = () => {
         const todoList = document.getElementById('todo-list');
@@ -10,18 +10,24 @@ const render = () => {
         elemento.parentNode.removeChild(elemento)
         // Clean the array: 
         todos.splice(i, 1);
+        actualiza_todos(todos)
         render() 
         })
     }
     )
 }
-
+const actualiza_todos = (todos) => {
+    const todoString = JSON.stringify(todos);
+    localStorage.setItem('todos', todoString);
+}
 const form = document.getElementById('todo-form');
+render()
 form.onsubmit = (x) =>{
     x.preventDefault();
     const todo = document.getElementById('todo'); 
     const todotext = todo.value;
     todo.value = ''; 
     todos.push(todotext);
+    actualiza_todos(todos);
     render(); 
     }
